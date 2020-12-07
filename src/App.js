cb14b97285ef19a3ca1e0ff15a5cfceb7854745b
Ya-dola <Ya-dola@users.noreply.github.com>
2020-12-07 17:15:52 +0300

    Moving Theme Change to Home Component

--- src/App.js
+++ src/App.js
@@ -2,31 +2,13 @@ import React from 'react';
 import {BrowserRouter, Route} from "react-router-dom";
 import Home from "./Home";
 import './App.css';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
-    const preferredColorScheme = useMediaQuery('(prefers-color-scheme: dark)');
-
-    const theme = React.useMemo(
-        () =>
-            createMuiTheme({
-                palette: {
-                    type: preferredColorScheme ? 'dark' : 'light',
-                },
-            }),
-        [preferredColorScheme],
-    );
-
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <BrowserRouter>
-                <Route exact path="/" component={Home}/>
-                {/*<Route exact path="/YadolaGame/:gameId" component={ChampionHeatmap}/>*/}
-            </BrowserRouter>
-        </ThemeProvider>
+        <BrowserRouter>
+            <Route exact path="/" component={Home}/>
+            {/*<Route exact path="/YadolaGame/:gameId" component={ChampionHeatmap}/>*/}
+        </BrowserRouter>
     );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,34 @@ import React, {Fragment} from 'react';
 import Box from '@material-ui/core/Box';
 import HeaderBar from "./HeaderBar";
 import GamesGridList from "./GamesGridList";
+import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 function Home() {
+    const preferredColorScheme = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = React.useMemo(
+        () =>
+            createMuiTheme({
+                palette: {
+                    type: preferredColorScheme ? 'dark' : 'light',
+                },
+            }),
+        [preferredColorScheme],
+    );
+
     return (
-        <Fragment>
-            <HeaderBar title={"Yadola Games"}/>
-            <Box mx="auto" p={2} overflow="hidden">
-                <GamesGridList/>
-            </Box>
-        </Fragment>
+
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Fragment>
+                <HeaderBar title={"Yadola Games"}/>
+                <Box mx="auto" p={2} overflow="hidden">
+                    <GamesGridList/>
+                </Box>
+            </Fragment>
+        </ThemeProvider>
     );
 }
 
